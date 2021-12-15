9cde6bc1c076dd9c67a196be47d1f68519ee7e6e
aasfaw95 <abyssiniaasfaw@gmail.com>
2021-12-15 12:57:24 -0600

    Project created

---
+++ dungeon-notes/src/main/java/abyssiniaasfaw/dungeonnotes/DungeonNotesApplication.java
@@ -0,0 +1,13 @@
+package abyssiniaasfaw.dungeonnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonNotesApplication.class, args);
+	}
+
+}
---
+++ dungeon-notes/src/test/java/abyssiniaasfaw/dungeonnotes/DungeonNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package abyssiniaasfaw.dungeonnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
