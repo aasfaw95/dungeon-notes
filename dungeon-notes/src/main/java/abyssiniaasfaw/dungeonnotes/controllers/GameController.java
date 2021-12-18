471093cf48578dbd1a11943e16a8e972c510ea36
aasfaw95 <abyssiniaasfaw@gmail.com>
2021-12-18 12:00:07 -0600

    added css and js files

--- dungeon-notes/src/main/java/abyssiniaasfaw/dungeonnotes/controllers/GameController.java
+++ dungeon-notes/src/main/java/abyssiniaasfaw/dungeonnotes/controllers/GameController.java
@@ -1,12 +1,12 @@
 package abyssiniaasfaw.dungeonnotes.controllers;
 
+import abyssiniaasfaw.dungeonnotes.models.Game;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +14,7 @@ import java.util.List;
 @RequestMapping("games")
 public class GameController {
 
-    private static List<String> games = new ArrayList<>();
+    private static List<Game> games = new ArrayList<>();
 
     @GetMapping
     public String displayAllGames(Model model) {
@@ -29,7 +29,7 @@ public class GameController {
 
     @PostMapping("create")
     public String createGame(@RequestParam String gameName) {
-        games.add(gameName);
+        games.add(new Game(gameName));
         return "redirect:";
     }
 }
