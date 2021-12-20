2e94880f80d5e9ad32f6079784faabaf161b7210
aasfaw95 <abyssiniaasfaw@gmail.com>
2021-12-20 12:51:40 -0600

    added an id for each individual game

--- dungeon-notes/src/main/java/abyssiniaasfaw/dungeonnotes/models/Game.java
+++ dungeon-notes/src/main/java/abyssiniaasfaw/dungeonnotes/models/Game.java
@@ -1,6 +1,11 @@
 package abyssiniaasfaw.dungeonnotes.models;
 
+import java.util.Objects;
+
 public class Game {
+
+    private int id;
+    private static int nextId = 1;
 
     private String name;
     private String description;
@@ -8,6 +13,8 @@ public class Game {
     public Game(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -26,8 +33,25 @@ public class Game {
         this.description = description;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return id == game.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
